2e0bb730629debbe74a6953788936c66ca503f78
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-06-18 23:16:27 +0100

    lines changed

--- I4G003670P82/I4G003670P82/asgi.py
+++ I4G003670P82/I4G003670P82/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for  I4G009584AYP project.
+ASGI config for  I4G003670P82 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G009584AYP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G003670P82.settings')
 
 application = get_asgi_application()
